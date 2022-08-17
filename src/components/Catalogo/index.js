4bd8c854a041094055cb611577d93fddc7a6d0fe
VictorMont03 <victormonteir2@gmail.com>
2022-08-17 14:46:29 -0300

    victor features

--- src/components/Catalogo/index.js
+++ src/components/Catalogo/index.js
@@ -1,163 +1,164 @@
-import React, {Fragment,useState} from 'react';
-import './style.css';
-import {Button} from "@mui/material";
+import React, { Fragment, useState } from "react";
+import "./style.css";
+import { Button } from "@mui/material";
 
-import Menu from '../menu';
+import Menu from "../menu";
 
 const Catalogo = () => {
+  const [page, setPage] = useState(false);
+  const [single, setSingle] = useState();
 
-            const[page,setPage] = useState(false)
-            const [single,setSingle] = useState() 
-            
-            function getMovie(f) {
-                setSingle(f)
-                setPage(true)
-            }
+  function getMovie(f) {
+    setSingle(f);
+    setPage(true);
+  }
 
-        const data = [
-            {
-            titulo: 'Baita 1',
-            horario: '12:05',
-            sinopse:'sinopse',
-            img:'https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg',
-            genero:'Binare',
-            dub:'Dublado',
-            bebidas:'Agua',
-            comidas:'Pipoca',
-            chairs:[
-                {"id": 1, used: true},
-                {"id": 2, used: false},
-                {"id": 3, used: true},
-                {"id": 4, used: true},
-                {"id": 5, used: false},
-                {"id": 6, used: true},
-                {"id": 7, used: true},
-                {"id": 8, used: false},
-                {"id": 9, used: true},
-                {"id": 10, used: true},
-                {"id": 11, used: true},
-                {"id": 12, used: false},
-                {"id": 13, used: true},
-                {"id": 14, used: true},
-                {"id": 15, used: false},
-                {"id": 16, used: true},
-                {"id": 17, used: true},
-                {"id": 18, used: false},
-                {"id": 19, used: true},
-                {"id": 20, used: true},
-                {"id": 21, used: true},
-                {"id": 22, used: false},
-                {"id": 23, used: true},
-                {"id": 24, used: true},
-                {"id": 25, used: false},
-                {"id": 26, used: true},
-                {"id": 27, used: true},
-            ]
-            },
-            {
-                titulo: 'Baita 2',
-                horario: '12:05',
-                sinopse:'sinopse',
-                img:'https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg',
-                genero:'ação/aventura',
-                dub:'Legendado',
-                bebidas:'Agua',
-                comidas:'Pipoca',
-                chairs:[
-                    {"id": 1, used: true},
-                    {"id": 2, used: false},
-                    {"id": 3, used: true},
-                    {"id": 4, used: true},
-                    {"id": 5, used: false},
-                    {"id": 6, used: true},
-                    {"id": 7, used: true},
-                    {"id": 8, used: false},
-                    {"id": 9, used: true},
-                    {"id": 10, used: true},
-                    {"id": 11, used: true},
-                    {"id": 12, used: false},
-                    {"id": 13, used: true},
-                    {"id": 14, used: true},
-                    {"id": 15, used: false},
-                    {"id": 16, used: true},
-                    {"id": 17, used: true},
-                    {"id": 18, used: false},
-                    {"id": 19, used: true},
-                    {"id": 20, used: true},
-                    {"id": 21, used: true},
-                    {"id": 22, used: false},
-                    {"id": 23, used: true},
-                    {"id": 24, used: true},
-                    {"id": 25, used: false},
-                    {"id": 26, used: true},
-                    {"id": 27, used: true},
-                ]
-                },
-                {
-                titulo: 'Baita 3',
-                horario: '12:05',
-                sinopse:'sinopse',
-                img:'https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg',
-                genero:'Suspense',
-                dub:'Dublado',
-                bebidas:'Agua',
-                comidas:'Pipoca',
-                chairs:[
-                    {"id": 1, used: true},
-                    {"id": 2, used: false},
-                    {"id": 3, used: true},
-                    {"id": 4, used: true},
-                    {"id": 5, used: false},
-                    {"id": 6, used: true},
-                    {"id": 7, used: true},
-                    {"id": 8, used: false},
-                    {"id": 9, used: true},
-                    {"id": 10, used: true},
-                    {"id": 11, used: true},
-                    {"id": 12, used: false},
-                    {"id": 13, used: true},
-                    {"id": 14, used: true},
-                    {"id": 15, used: false},
-                    {"id": 16, used: true},
-                    {"id": 17, used: true},
-                    {"id": 18, used: false},
-                    {"id": 19, used: true},
-                    {"id": 20, used: true},
-                    {"id": 21, used: true},
-                    {"id": 22, used: false},
-                    {"id": 23, used: true},
-                    {"id": 24, used: true},
-                    {"id": 25, used: false},
-                    {"id": 26, used: true},
-                    {"id": 27, used: true},
-                    ]
-                    },
-        
-    ]
+  const data = [
+    {
+      titulo: "Baita 1",
+      horario: "12:05",
+      sinopse: "sinopse",
+      img: "https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg",
+      genero: "Binare",
+      dub: "Dublado",
+      bebidas: "Agua",
+      comidas: "Pipoca",
+      chairs: [
+        { id: 1, used: true },
+        { id: 2, used: false },
+        { id: 3, used: true },
+        { id: 4, used: true },
+        { id: 5, used: false },
+        { id: 6, used: true },
+        { id: 7, used: true },
+        { id: 8, used: false },
+        { id: 9, used: true },
+        { id: 10, used: true },
+        { id: 11, used: true },
+        { id: 12, used: false },
+        { id: 13, used: true },
+        { id: 14, used: true },
+        { id: 15, used: false },
+        { id: 16, used: true },
+        { id: 17, used: true },
+        { id: 18, used: false },
+        { id: 19, used: true },
+        { id: 20, used: true },
+        { id: 21, used: true },
+        { id: 22, used: false },
+        { id: 23, used: true },
+        { id: 24, used: true },
+        { id: 25, used: false },
+        { id: 26, used: true },
+        { id: 27, used: true },
+      ],
+    },
+    {
+      titulo: "Baita 2",
+      horario: "12:05",
+      sinopse: "sinopse",
+      img: "https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg",
+      genero: "ação/aventura",
+      dub: "Legendado",
+      bebidas: "Agua",
+      comidas: "Pipoca",
+      chairs: [
+        { id: 1, used: true },
+        { id: 2, used: false },
+        { id: 3, used: true },
+        { id: 4, used: true },
+        { id: 5, used: false },
+        { id: 6, used: true },
+        { id: 7, used: true },
+        { id: 8, used: false },
+        { id: 9, used: true },
+        { id: 10, used: true },
+        { id: 11, used: true },
+        { id: 12, used: false },
+        { id: 13, used: true },
+        { id: 14, used: true },
+        { id: 15, used: false },
+        { id: 16, used: true },
+        { id: 17, used: true },
+        { id: 18, used: false },
+        { id: 19, used: true },
+        { id: 20, used: true },
+        { id: 21, used: true },
+        { id: 22, used: false },
+        { id: 23, used: true },
+        { id: 24, used: true },
+        { id: 25, used: false },
+        { id: 26, used: true },
+        { id: 27, used: true },
+      ],
+    },
+    {
+      titulo: "Baita 3",
+      horario: "12:05",
+      sinopse: "sinopse",
+      img: "https://cdn.olivre.com.br/wp-content/uploads/2019/09/20190912-y5i92dqjh3m31.jpg",
+      genero: "Suspense",
+      dub: "Dublado",
+      bebidas: "Agua",
+      comidas: "Pipoca",
+      chairs: [
+        { id: 1, used: true },
+        { id: 2, used: false },
+        { id: 3, used: true },
+        { id: 4, used: true },
+        { id: 5, used: false },
+        { id: 6, used: true },
+        { id: 7, used: true },
+        { id: 8, used: false },
+        { id: 9, used: true },
+        { id: 10, used: true },
+        { id: 11, used: true },
+        { id: 12, used: false },
+        { id: 13, used: true },
+        { id: 14, used: true },
+        { id: 15, used: false },
+        { id: 16, used: true },
+        { id: 17, used: true },
+        { id: 18, used: false },
+        { id: 19, used: true },
+        { id: 20, used: true },
+        { id: 21, used: true },
+        { id: 22, used: false },
+        { id: 23, used: true },
+        { id: 24, used: true },
+        { id: 25, used: false },
+        { id: 26, used: true },
+        { id: 27, used: true },
+      ],
+    },
+  ];
 
-    return(
-        <Fragment>
-            {page ? <Menu show={setPage} data={single}/> : <div className="catalogo-style"> 
-                 <h1>Filmes em Cartaz</h1>
-                    <div className="catalogo-org">     
-                         {data.map((f) => (
-                            <>
-                                <h2>{f.titulo}</h2>
-                         <div style={{display: 'flex',alignItems:'center'}}>
-                             <Button onClick={()=>getMovie(f)}>{f.horario}</Button>
-                         </div>
-                         <div style={{display: 'flex',alignItems:'center'}}>
-                         <img src = {f.img}/>  
-                             <h3>{f.sinopse}</h3>
-                             <h3></h3>
-                         </div>
-                            </>
-                         ))}                    
-    </div>
-    </div>}
+  return (
+    <Fragment>
+      {page ? (
+        <Menu show={setPage} data={single} />
+      ) : (
+        <div className="catalogo-style">
+          <h1>Filmes em Cartaz</h1>
+          <div className="catalogo-org">
+            {data.map((f) => (
+              <>
+                <h2>{f.titulo}</h2>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <Button onClick={() => getMovie(f)}>{f.horario}</Button>
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <img src={f.img} />
+                  <h3>{f.sinopse}</h3>
+                  <h3></h3>
+                </div>
+              </>
+            ))}
+          </div>
+        </div>
+      )}
     </Fragment>
-    )
-    
-}
+  );
+};
 
 export default Catalogo;
--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,99 +1,138 @@
-import React, {Fragment, useState} from 'react';
-import {Button} from "@mui/material";
-import {MdChair} from 'react-icons/md'; 
-// import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import Modal from '@mui/material/Modal';
-import './style.css';
+import React, { Fragment, useState, useEffect } from "react";
+import { Button } from "@mui/material";
+import { MdChair } from "react-icons/md";
 
-import waterImg from '../../images/water.png';
-import popcornImg from '../../images/popcorn.png';
+import "./style.css";
 
-const Menu = ({data, show}) => {
+import waterImg from "../../images/water.png";
+import popcornImg from "../../images/popcorn.png";
 
-    function BasicModal() {
-        const [open, setOpen] = React.useState(false);
-        const handleOpen = () => setOpen(true);
-        const handleClose = () => setOpen(false);
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  display: "flex",
+  p: 4,
+};
+
+const Menu = ({ data, show }) => {
+  const [assentos, setAssentos] = useState([]);
+
+  function pushChairs(e) {
+    setAssentos((a) => [...a, e]);
+    console.log(assentos);
+  }
+
+  const [countWater, setCountWater] = useState(0);
+
+  function decreaseWater() {
+    if (countWater - 1 >= 0) {
+      setCountWater(countWater - 1);
     }
+  }
 
-    const [assentos, setAssentos] = useState([]);
+  const [countPop, setCountPop] = useState(0);
 
-    function pushChairs(e){
-        setAssentos((a) => [...a, e])
-        console.log(assentos);
+  function decreasePop() {
+    if (countPop - 1 >= 0) {
+      setCountPop(countPop - 1);
     }
+  }
 
-    const[countWater,setCountWater] = useState(0);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    function decreaseWater() {
-        if (countWater - 1 >= 0) {
-            setCountWater(countWater - 1);
-        }
-    }
+  useEffect(() => {
+    console.log("mudei o modal");
+  }, []);
 
-    const[countPop,setCountPop] = useState(0);
-
-    function decreasePop() {
-        if(countPop -1 >=  0) {
-                setCountPop(countPop -1);
-        }
-    }
-
-    return(
+  return (
     <Fragment>
-        <Button variant="text" onClick={() => show(false)}>voltar</Button>
-        <div className="menu-container">
-        <img src = {data.img}/> 
-            <div style={{display: 'flex',flexDirection: 'column'}}> 
-                <h1>Filme: {data.titulo}</h1>
-                <span>{data.dub}</span>
-            </div>
-                <div>
-                    <h1>Sessão: {data.horario}</h1>
-                    <span>Gênero:{data.genero}</span>
-                    
-             </div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <section
+          sx={style}
+          className="modal-card"
+          onClick={() => handleClose()}
+        >
+          <div>
+            <h1>Text in a modal</h1>
+            <p>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </p>
+          </div>
+        </section>
+      </Modal>
+      <Button variant="text" onClick={() => show(false)}>
+        voltar
+      </Button>
+      <div className="menu-container">
+        <img src={data.img} />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h1>Filme: {data.titulo}</h1>
+          <span>{data.dub}</span>
         </div>
-        <div className="assentos-container"> 
-            <span>
-                Escolha seus assentos :
-            </span>  
-            <div className="chairs-container">
-                {data.chairs.map((c)=>{
-                    return( 
-                    <div className="cadeirinha-div" onClick={(e) => pushChairs(c)}> 
-                            <MdChair size="30px" color={c.used ? "#f6f6f6" : "#008000"}/>
-                            <span className="cadeirinha">{c.id}</span>
-
-                    </div>)
-                })}
-            </div>
+        <div>
+          <h1>Sessão: {data.horario}</h1>
+          <span>Gênero:{data.genero}</span>
         </div>
-        <span>Você deseja algum acompanhamento para a sessão?</span>
-        <div className="store-container">
-                    <div>
-                         <img style={{width: '50px'}} src={waterImg}/>                    
-                        <p>{countWater} Água 250ml  </p>
-                        <Button onClick={() => setCountWater(countWater+1)}> +</Button> 
-                        <Button onClick={() => decreaseWater()}>-</Button>    
-                    </div>
-                    <div>
-                         <img style={{width: '50px'}} src={popcornImg}/>
-                         <p>{countPop} Pipoca</p>
-                         <Button onClick={() => setCountPop(countPop+1 )}> +</Button>
-                         <Button onClick={() => decreasePop()}> - </Button>
-                    </div>
-                    <Button >Open modal</Button>
+      </div>
+      <div className="assentos-container">
+        <span>Escolha seus assentos :</span>
+        <div className="chairs-container">
+          {data.chairs.map((c) => {
+            return (
+              <div className="cadeirinha-div" onClick={(e) => pushChairs(c)}>
+                <MdChair size="30px" color={c.used ? "#f6f6f6" : "#008000"} />
+                <span className="cadeirinha">{c.id}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <span>Você deseja algum acompanhamento para a sessão?</span>
+      <div className="store-container">
+        <div>
+          <img style={{ width: "50px" }} src={waterImg} />
+          <p>{countWater} Água 250ml </p>
+          <Button onClick={() => setCountWater(countWater + 1)}> +</Button>
+          <Button onClick={() => decreaseWater()}>-</Button>
+        </div>
+        <div>
+          <img style={{ width: "50px" }} src={popcornImg} />
+          <p>{countPop} Pipoca</p>
+          <Button onClick={() => setCountPop(countPop + 1)}> +</Button>
+          <Button onClick={() => decreasePop()}> - </Button>
+        </div>
 
-                    <div>      
-    </div>
-                </div>
-                
+        <div></div>
+      </div>
+      <div>
+        {assentos.map((ass, i) => (
+          <Fragment>
+            <p>Assentos selecionados:</p>
+            <br />
+            <p>{ass.id}</p>
+          </Fragment>
+        ))}
+      </div>
     </Fragment>
-    )
-
-}
+  );
+};
 export default Menu;
 
-//como salvar nos cookies 
-//fazer a porra da pasgina 
+//como salvar nos cookies
+//fazer a porra da pasgina
